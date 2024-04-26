81bbdaf553f2eb0bbe971a0a594ae37304809aa3
tdub0 <43589631+tdub0@users.noreply.github.com>
2024-04-26 07:57:39 -0700

    add taskwarrior-tui

--- .config/yadm/tasks.sh
+++ .config/yadm/tasks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-_version="2.6.2"
+_tw_version="2.6.2"
+_tw_tui_version="0.25.4"
 _resource_dir=~/.local/resources
 _install_dir=~/.local/
 
@@ -11,16 +12,20 @@ sudo dnf install libuuid-devel -y || return
 
 echo "Installing Taskwarrior"
 pushd $_resource_dir || return
-curl -fLO https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${_version}/task-${_version}.tar.gz
-tar xzf task-${_version}.tar.gz
-echo "Untar complete"
-cd task-${_version} || return
+curl -fLOsS https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${_tw_version}/task-${_tw_version}.tar.gz
+tar xzf task-${_tw_version}.tar.gz
+cd task-${_tw_version} || return
 cmake -DCMAKE_BUILD_TYPE=release -DENABLE_SYNC=OFF -DCMAKE_INSTALL_PREFIX=${_install_dir} .
 make
 sudo make install
 echo "Installation complete"
 cd .. || return
-rm -rf task-${_version}/
-rm task-${_version}.tar.gz
-echo "Removal complete"
+rm -rf task-${_tw_version}/
+rm task-${_tw_version}.tar.gz
+echo "Installing taskwarrior-tui"
+curl -fLOsS https://github.com/kdheepak/taskwarrior-tui/releases/download/v${_tw_tui_version}/taskwarrior-tui-x86_64-unknown-linux-gnu.tar.gz
+tar xzf taskwarrior-tui-x86_64-unknown-linux-gnu.tar.gz
+mv ./taskwarrior-tui ${_install_dir}/bin/
+rm taskwarrior-tui-x86_64-unknown-linux-gnu.tar.gz
+echo "Installation complete"
 popd || return

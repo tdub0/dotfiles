63a3267ebdb0701c0e6b404ad57a169862aea0c2
tdub0 <43589631+tdub0@users.noreply.github.com>
2026-03-06 11:33:32 -0700

    Fix basedpyright and harper_ls config

--- .config/nvim/lsp/basedpyright.lua
+++ .config/nvim/lsp/basedpyright.lua
@@ -2,8 +2,6 @@ vim.lsp.config("basedpyright", {
   settings = {
     basedpyright = {
       disableOrganizeImports = true,
-    },
-    python = {
       analysis = {
         ignore = { "*" },
       },
